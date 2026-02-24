bcca1c4f8366f2952a60fc6505590f1826276c59
TaianeAlbuquerqueDev <taiane.albuquerque.1994@gmail.com>
2026-02-24 11:00:23 -0300

    primeiro commit

---
+++ src/main/java/br/com/senai/api_clinica_veterinaria/ApiClinicaVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_clinica_veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicaVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicaVeterinariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/api_clinica_veterinaria/ApiClinicaVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_clinica_veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicaVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
